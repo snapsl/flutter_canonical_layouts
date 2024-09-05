fd8513c125235b60a9cffb94d67dddefe4198d58
snapsl <snapsl@users.noreply.github.com>
2024-09-05 20:50:01 +0200

    Add orientation to itemcard

--- example/lib/widgets/item_card.dart
+++ example/lib/widgets/item_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 Color colorFromIndex(int index) =>
     Colors.primaries[index % Colors.primaries.length];
 
+/// Simple Card Widget
 class ItemCard extends StatelessWidget {
   final Color color;
 
@@ -10,34 +11,55 @@ class ItemCard extends StatelessWidget {
 
   final String subtitle;
 
+  final Orientation orientation;
+
   const ItemCard({
     super.key,
     required this.color,
     required this.title,
     required this.subtitle,
+    this.orientation = Orientation.portrait,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.zero,
       clipBehavior: Clip.hardEdge,
       child: InkWell(
-        onTap: () {},
-        child: Column(children: [
-          Expanded(
-            child: Card(
-              color: color,
-              child: const SizedBox.expand(),
-            ),
-          ),
-          ListTile(
-            title: Text(title),
-            subtitle: Text(subtitle),
-            isThreeLine: true,
-          )
-        ]),
-      ),
+          onTap: () {},
+          child: switch (orientation) {
+            Orientation.portrait => Column(children: [
+                Expanded(
+                  child: Card(
+                    color: color,
+                    margin: EdgeInsets.zero,
+                    child: const SizedBox.expand(),
+                  ),
+                ),
+                ListTile(
+                  title: Text(title),
+                  subtitle: Text(subtitle),
+                  isThreeLine: true,
+                )
+              ]),
+            Orientation.landscape => Row(children: [
+                Expanded(
+                  child: Card(
+                    color: color,
+                    margin: EdgeInsets.zero,
+                    child: const SizedBox.expand(),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ListTile(
+                    title: Text(title),
+                    subtitle: Text(subtitle),
+                    isThreeLine: true,
+                  ),
+                )
+              ]),
+          }),
     );
   }
 }
