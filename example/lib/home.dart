9473cb5b00c8fecfa09b61e5f346b3532794ab12
snapsl <snapsl@users.noreply.github.com>
2024-09-05 20:35:07 +0200

    Add fab to home screen

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+
+import 'widgets/edit_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'This is an exemplary app for implementing material canonical layouts in Flutter.',
+    return Scaffold(
+      appBar: AppBar(),
+      body: Card(
+        elevation: 0,
+        color: Theme.of(context).colorScheme.surfaceContainerLowest,
+        child: const Center(
+          child: Text(
+            'This is an exemplary app for implementing material canonical layouts in Flutter.',
+          ),
         ),
       ),
+      // Note: show FAB on small screens.
+      floatingActionButton:
+          Breakpoints.small.isActive(context) ? const EditButton() : null,
     );
   }
 }
