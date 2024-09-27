fdd9c936d775297fbf1545f8248d535c9eaa7952
snapsl <snapsl@users.noreply.github.com>
2024-09-27 11:48:03 +0200

    Adaptive snackbar size

--- example/lib/widgets/edit_button.dart
+++ example/lib/widgets/edit_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 
 class EditButton extends StatelessWidget {
   final bool extended;
@@ -9,11 +10,21 @@ class EditButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO SnackBar in front of BottomNavigation
     void onPressed() => ScaffoldMessenger.of(context)
       ..hideCurrentSnackBar()
       ..showSnackBar(
-        const SnackBar(
-          content: Text('Yay! A SnackBar!'),
+        SnackBar(
+          margin: EdgeInsetsDirectional.only(
+            start: 16,
+            top: 14,
+            end: Breakpoints.largeAndUp.isActive(context)
+                ? (360 - MediaQuery.sizeOf(context).width).abs() - 16
+                : 16,
+            bottom: 14,
+          ),
+          content: const Text('Yay! A SnackBar!'),
+          behavior: SnackBarBehavior.floating,
         ),
       );
 
